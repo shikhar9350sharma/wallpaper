9df82e03ff48dafcbe046687e7c5e1bda4bac219
shikhar9350sharma <shikharsharma593@gmail.com>
2026-04-22 14:17:12 +0530

    initial commit

--- app/category/[slug]/page.js
+++ app/category/[slug]/page.js
@@ -1,26 +1,34 @@
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
 
-const categories = ['cars', 'gaming', 'anime'];
+const categories = ['cars', 'gaming', 'anime', 'minimal', 'nature' ];
 
 const wallpapers = {
   cars: [
     { id: 1, title: 'Sports Car', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/46.jpg', downloads: 1234 },
-    { id: 2, title: 'Luxury Sedan', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/44.jpg', downloads: 987 },
+    { id: 2, title: 'Luxury Sedan', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/44.jpg', downloads: 94327 },
   ],
   gaming: [
     { id: 3, title: 'Cyberpunk City', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/1.jpeg', downloads: 2345 },
-    { id: 4, title: 'Fantasy Landscape', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/44.jpg', downloads: 1567 },
+    { id: 4, title: 'Fantasy Landscape', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/44.jpg', downloads: 1267 },
   ],
   anime: [
     { id: 5, title: 'Anime Character', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/46.jpg', downloads: 3456 },
-    { id: 6, title: 'Anime Scene', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/49.jpg', downloads: 2109 },
+    { id: 6, title: 'Anime Scene', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/49.jpg', downloads: 213409 },
+  ],
+  minimal: [
+    { id: 7, title: 'A Boy', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/32.jpg?updatedAt=1776844234556', downloads: 54356 },
+    { id: 8, title: 'The Journey', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/43.jpg?updatedAt=1776844205634', downloads: 334109 },
+  ],
+  nature: [
+    { id: 9, title: 'A Boy', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/37.jpg', downloads: 3456 },
+    { id: 10, title: 'The Journey', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/5.png', downloads: 34309 },
+    { id: 11, title: 'The House', image: 'https://ik.imagekit.io/hopetoseeyouagain/Wallpaper/4.png', downloads: 54309 },
   ],
 };
 
-// NOTICE: Added 'async' here
 export default async function CategoryPage({ params }) {
-  // NOTICE: Added 'await' here
+
   const { slug } = await params;
 
   if (!categories.includes(slug)) {
